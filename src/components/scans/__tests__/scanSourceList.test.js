f2b0ba2164af91080b34c50c726ff668b104f5a2
CD Cabrera <ccabrera@redhat.com>
2019-05-03 14:46:02 -0400

    DEV component, scanSourceLIst
    * scanSourceList, update towards selector use, unit test

--- src/components/scans/__tests__/scanSourceList.test.js
+++ src/components/scans/__tests__/scanSourceList.test.js
@@ -1,26 +1,72 @@
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
-import { shallow } from 'enzyme';
-import ScanSourceList from '../scanSourceList';
+import { mount, shallow } from 'enzyme';
+import { ConnectedScanSourceList, ScanSourceList } from '../scanSourceList';
+
 
 describe('ScanSourceList Component', () => {
-  const generateEmptyStore = () => configureMockStore()({});
+  const generateEmptyStore = (obj = {}) => configureMockStore()(obj);
 
-  it('should shallow render a basic component with status', () => {
-    const store = generateEmptyStore();
+  it('should render a connected component', () => {
+    const store = generateEmptyStore({ scansAction: { job: {} } });
+
     const props = {
-      scan: {
-        sources: [
-          {
-            id: 15,
-            name: 'TestSource',
-            source_type: 'network'
-          }
-        ]
-      }
+      id: 1
     };
-    const wrapper = shallow(<ScanSourceList {...props} />, { context: { store } });
 
-    expect(wrapper.dive()).toMatchSnapshot();
+    const component = shallow(<ConnectedScanSourceList {...props} />, {
+      context: { store }
+    });
+
+    expect(component).toMatchSnapshot('connected');
+  });
+
+  it('should render a non-connected component', () => {
+    const props = {
+      id: 1,
+      scanJobList: [
+        {
+          connectTaskStatus: 'lorem',
+          connectTaskStatusMessage: 'ipsum',
+          id: 1,
+          inspectTaskStatus: 'dolor',
+          inspectTaskStatusMessage: 'sit',
+          name: 'test',
+          sourceType: ''
+        }
+      ]
+    };
+
+    const component = mount(<ScanSourceList {...props} />);
+
+    expect(component.render()).toMatchSnapshot('non-connected');
+  });
+
+  it('should handle multiple status messages', () => {
+    const props = {
+      id: 1,
+      scanJobList: [
+        {
+          connectTaskStatus: 'pending',
+          connectTaskStatusMessage: 'ipsum',
+          id: 1,
+          inspectTaskStatus: 'dolor',
+          inspectTaskStatusMessage: 'sit',
+          name: 'test',
+          sourceType: ''
+        }
+      ]
+    };
+
+    let component = shallow(<ScanSourceList {...props} />);
+    expect(component).toMatchSnapshot('connect status');
+
+    props.scanJobList[0].connectTaskStatus = 'completed';
+    component = shallow(<ScanSourceList {...props} />);
+    expect(component).toMatchSnapshot('inspect status');
+
+    props.scanJobList[0].inspectTaskStatus = undefined;
+    component = shallow(<ScanSourceList {...props} />);
+    expect(component).toMatchSnapshot('fallback status');
   });
 });
