5f54b179e961b9adef36d1f215e24d935a1a55d8
CD Cabrera <ccabrera@redhat.com>
2019-05-07 01:43:40 -0400

    DEV issue 46, redux and services
    * redux, reducers, actions, selectors for reports
    * reportsService, remove unused functions
    * helpers, add downloadPackage function for report tar.gz
    * apiConstants, scan jobs, reports additions

--- src/redux/reducers/reportsReducer.js
+++ src/redux/reducers/reportsReducer.js
@@ -1,149 +1,20 @@
 import { reportsTypes } from '../constants';
-import { helpers } from '../../common/helpers';
 import { reduxHelpers } from '../common/reduxHelpers';
 
 const initialState = {
-  report: {
-    error: false,
-    errorMessage: '',
-    pending: false,
-    fulfilled: false,
-    reports: []
-  },
-
-  reports: {
-    error: false,
-    errorMessage: '',
-    pending: false,
-    fulfilled: false
-  },
-
-  merge: {
-    error: false,
-    errorMessage: '',
-    pending: false,
-    fulfilled: false
-  }
+  mergeReport: {},
+  reportsDownload: {}
 };
 
-const reportsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case reduxHelpers.REJECTED_ACTION(reportsTypes.GET_REPORT):
-      return reduxHelpers.setStateProp(
-        'report',
-        {
-          error: action.error,
-          errorMessage: helpers.getMessageFromResults(action.payload).message
-        },
-        {
-          state,
-          initialState
-        }
-      );
-
-    case reduxHelpers.REJECTED_ACTION(reportsTypes.GET_REPORTS):
-      return reduxHelpers.setStateProp(
-        'reports',
-        {
-          error: action.error,
-          errorMessage: helpers.getMessageFromResults(action.payload).message
-        },
-        {
-          state,
-          initialState
-        }
-      );
-
-    case reduxHelpers.REJECTED_ACTION(reportsTypes.GET_MERGE_REPORT):
-      return reduxHelpers.setStateProp(
-        'merge',
-        {
-          error: action.error,
-          errorMessage: helpers.getMessageFromResults(action.payload).message
-        },
-        {
-          state,
-          initialState
-        }
-      );
-
-    case reduxHelpers.PENDING_ACTION(reportsTypes.GET_REPORT):
-      return reduxHelpers.setStateProp(
-        'report',
-        {
-          pending: true
-        },
-        {
-          state,
-          initialState
-        }
-      );
-
-    case reduxHelpers.PENDING_ACTION(reportsTypes.GET_REPORTS):
-      return reduxHelpers.setStateProp(
-        'reports',
-        {
-          pending: true
-        },
-        {
-          state,
-          initialState
-        }
-      );
-
-    case reduxHelpers.PENDING_ACTION(reportsTypes.GET_MERGE_REPORT):
-      return reduxHelpers.setStateProp(
-        'merge',
-        {
-          pending: true
-        },
-        {
-          state,
-          initialState
-        }
-      );
-
-    case reduxHelpers.FULFILLED_ACTION(reportsTypes.GET_REPORT):
-      return reduxHelpers.setStateProp(
-        'report',
-        {
-          fulfilled: true,
-          reports: action.payload.data
-        },
-        {
-          state,
-          initialState
-        }
-      );
-
-    case reduxHelpers.FULFILLED_ACTION(reportsTypes.GET_REPORTS):
-      return reduxHelpers.setStateProp(
-        'reports',
-        {
-          fulfilled: true
-        },
-        {
-          state,
-          initialState
-        }
-      );
-
-    case reduxHelpers.FULFILLED_ACTION(reportsTypes.GET_MERGE_REPORT):
-      return reduxHelpers.setStateProp(
-        'merge',
-        {
-          fulfilled: true
-        },
-        {
-          state,
-          initialState
-        }
-      );
-
-    default:
-      return state;
-  }
-};
+const reportsReducer = (state = initialState, action) =>
+  reduxHelpers.generatedPromiseActionReducer(
+    [
+      { ref: 'reportsDownload', type: reportsTypes.GET_REPORTS_DOWNLOAD },
+      { ref: 'mergeReport', type: reportsTypes.GET_MERGE_REPORT }
+    ],
+    state,
+    action
+  );
 
 reportsReducer.initialState = initialState;
 
