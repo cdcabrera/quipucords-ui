3e62a20bf61fbdc9b58a38a470bf69e32011e2be
CD Cabrera <ccabrera@redhat.com>
2022-08-12 02:36:57 -0400

    refactor(sources): discovery-154, pf4 table

---
+++ src/components/icon/icon.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Spinner } from '@patternfly/react-core';
+import {
+  CheckCircleIcon,
+  DisconnectedIcon,
+  ErrorCircleOIcon,
+  ExclamationTriangleIcon,
+  IdCardIcon,
+  PencilAltIcon,
+  PficonNetworkRangeIcon,
+  PficonSatelliteIcon,
+  PficonVcenterIcon,
+  TrashIcon,
+  UnknownIcon,
+  IconSize
+} from '@patternfly/react-icons';
+import {
+  global_Color_dark_100 as gray,
+  global_success_color_100 as green,
+  global_warning_color_100 as yellow,
+  global_danger_color_100 as red
+} from '@patternfly/react-tokens';
+
+/**
+ * App icon variants
+ *
+ * @type {{running: string, canceled: string, paused: string, unreachable: string, success: string, created: string,
+ *     pending: string, cancelled: string, completed: string, failed: string}}
+ */
+const IconVariant = {
+  completed: 'success',
+  success: 'success',
+  failed: 'failed',
+  canceled: 'failed',
+  cancelled: 'failed',
+  created: 'pending',
+  pending: 'pending',
+  running: 'pending',
+  idCard: 'idCard',
+  network: 'network',
+  paused: 'paused',
+  pencil: 'pencil',
+  satellite: 'satellite',
+  trash: 'trash',
+  unknown: 'unknown',
+  unreachable: 'unreachable',
+  vcenter: 'vcenter'
+};
+
+// case 'vcenter':
+//       return { type: 'pf', name: 'vcenter' };
+//     case 'network':
+//       return { type: 'pf', name: 'network-range' };
+//     case 'satellite':
+//       return { type: 'pf', name: 'satellite' };
+//     default:
+//       return { type: 'pf', name: '' };
+
+/**
+ * Return an icon
+ *
+ * @param {object} props
+ * @param {string} props.symbol
+ * @param {object} props.props
+ * @returns {React.ReactNode}
+ */
+const Icon = ({ symbol, ...props }) => {
+  switch (symbol) {
+    case IconVariant.failed:
+      return <ErrorCircleOIcon {...{ ...{ color: red.value }, ...props }} />;
+    case IconVariant.idCard:
+      return <IdCardIcon {...{ ...{ color: gray.value }, ...props }} />;
+    case IconVariant.network:
+      return <PficonNetworkRangeIcon {...props} />;
+    case IconVariant.paused:
+      return <ExclamationTriangleIcon {...{ ...{ color: yellow.value }, ...props }} />;
+    // return { type: 'pf', name: 'warning-triangle-o', classNames: [] };
+    case IconVariant.pencil:
+      return <PencilAltIcon {...props} />;
+    case IconVariant.pending:
+      return <Spinner isSVG {...{ ...{ size: IconSize.md }, ...props }} />;
+    // return { type: 'fa', name: 'spinner', classNames: ['fa-spin'] };
+    case IconVariant.satellite:
+      return <PficonSatelliteIcon {...props} />;
+    case IconVariant.success:
+      return <CheckCircleIcon {...{ ...{ color: green.value }, ...props }} />;
+    case IconVariant.trash:
+      return <TrashIcon {...props} />;
+    case IconVariant.unreachable:
+      return <DisconnectedIcon {...{ ...{ color: red.value }, ...props }} />;
+    // return { type: 'pf', name: 'disconnected', classNames: ['is-error'] };
+    case IconVariant.vcenter:
+      return <PficonVcenterIcon {...props} />;
+    case IconVariant.unknown:
+    default:
+      // console.error(`Unknown status: ${scanStatus}`);
+      // return { type: 'pf', name: 'unknown', classNames: [] };
+      return <UnknownIcon {...props} />;
+  }
+};
+
+/**
+ * Prop types
+ *
+ * @type {{symbol: string}}
+ */
+Icon.propTypes = {
+  symbol: PropTypes.oneOf([...Object.values(IconVariant)])
+};
+
+/**
+ * Default props
+ *
+ * @type {{symbol: null}}
+ */
+Icon.defaultProps = {
+  symbol: null
+};
+
+export { Icon as default, Icon, IconVariant };
