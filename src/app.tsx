e7cad46a9f18e3b9f4db93db16ebfacc153f3d1c
Nicole Aragao <63803651+nicolearagao@users.noreply.github.com>
2024-04-30 16:28:05 -0300

    style: ds-653 Organize src files into standard dir (#348)

--- src/app.tsx
+++ src/app.tsx
@@ -6,8 +6,37 @@
  */
 import React from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
+import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+import I18n from './components/i18n/I18n';
+import { useLocale } from './components/sessionContext/sessionProvider';
+import AppLayout from './components/viewLayout/viewLayout';
+import { AppRoutes } from './routes';
 import './app.css';
 
-const App: React.FC = () => 'hello world';
+const App: React.FC = () => {
+  const locale = useLocale();
+  if (localStorage.getItem('authToken')) {
+    axios.interceptors.request.use(
+      config => {
+        config.headers['Authorization'] = `Token ${localStorage.getItem('authToken')}`;
+        return config;
+      },
+      error => {
+        console.error('Failed to set config', error);
+      }
+    );
+  }
 
-export { App as default, App };
+  return (
+    <I18n locale={locale}>
+      <BrowserRouter>
+        <AppLayout>
+          <AppRoutes />
+        </AppLayout>
+      </BrowserRouter>
+    </I18n>
+  );
+};
+
+export default App;
