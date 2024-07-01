06c0188f17eda4d032c347ce8dada0d2db15c9f2
CD Cabrera <cabrera.code@gmail.com>
2024-06-30 23:14:49 -0400

    DEV clean up

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,21 +1,6 @@
-const {
-  // cssLoaderResolve,
-  // sassLoaderResolve,
-  EslintWebpackPlugin
-  // MiniCssExtractPlugin
-} = require('weldable/lib/packages');
+const { EslintWebpackPlugin } = require('weldable/lib/packages');
 
 module.exports = ({ SRC_DIR } = {}) => ({
-  /*
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc)ss$/i,
-        use: [MiniCssExtractPlugin.loader, cssLoaderResolve, sassLoaderResolve]
-      }
-    ]
-  },
-  */
   plugins: [
     new EslintWebpackPlugin({
       context: SRC_DIR,
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,31 +1,7 @@
-// const {
-//  sassLoaderResolve,
-//  MiniCssExtractPlugin
-// } = require('weldable/lib/packages');
-
 module.exports = () => ({
   ignoreWarnings: [
     {
       message: /mini-css-extract-plugin/
     }
   ]
-  /*
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc)ss$/i,
-        use: [sassLoaderResolve]
-      }
-    ]
-  }
-  */
-  /*
-  plugins: [
-    new MiniCssExtractPlugin({
-      chunkFilename: '[name].[contenthash:8].chunk.css',
-      filename: '[name].[contenthash:8].css',
-      ignoreOrder: true
-    })
-  ]
-  */
 });
