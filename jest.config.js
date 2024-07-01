34dabb2a8705be67bbdc1bfec04c8e8f89500851
CD Cabrera <cabrera.code@gmail.com>
2024-07-01 02:12:59 -0400

    DEV clean up jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/.*/**'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/.*/**', '!src/**/**/index.ts'],
   coverageThreshold: {
     global: {
       branches: 50,
