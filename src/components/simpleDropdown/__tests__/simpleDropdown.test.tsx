f5448ddd5c61510b53835c8712300e9a5452cc6c
CD Cabrera <cabrera.code@gmail.com>
2024-09-05 20:54:15 -0400

    DEV testing simpleDropdown, actionMenu

--- src/components/simpleDropdown/__tests__/simpleDropdown.test.tsx
+++ src/components/simpleDropdown/__tests__/simpleDropdown.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom';
 import { shallowComponent } from '../../../../config/jest.setupTests';
 import { SimpleDropdown } from '../simpleDropdown';
 
