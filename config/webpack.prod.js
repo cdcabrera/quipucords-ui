0fce972bf41d5daf2f390dcd0f9d6974499f1bb0
CD Cabrera <cabrera.code@gmail.com>
2024-09-13 14:58:27 -0400

    DEV webpack config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,7 +1,47 @@
-module.exports = () => ({
-  ignoreWarnings: [
-    {
-      message: /mini-css-extract-plugin/
+const fs = require('fs');
+const path = require('path');
+
+console.log('>>>>>>>>>>>>>>>>>>> WORK');
+
+const updateTsConfig = ({ filePath, addConfiguration, encoding = 'utf8' } = {}) => {
+  const currentConfig = require(filePath);
+  // const updatedContents = JSON.stringify({ ...currentConfig, ...addConfiguration }, null, 2);
+
+
+
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>');
+  // console.log(JSON.parse(JSON.stringify(currentConfig)));
+  // console.log();
+  // console.log(updatedContents);
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>');
+  // console.log(undefined);
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>');
+
+  try {
+    // const currentConfig = JSON.parse(fs.readFileSync(filePath, { encoding }));
+    const updatedContents = JSON.stringify({ ...currentConfig, ...addConfiguration }, null, 2);
+    fs.writeFileSync(filePath, updatedContents, { encoding });
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+module.exports = ({ RELATIVE_DIRNAME } = {}) => {
+  updateTsConfig({
+    filePath: path.resolve(process.cwd(), 'tsconfig.json'),
+    addConfiguration: {
+      exclude: [
+        '**/*.test.ts',
+        '**/*.test.tsx'
+      ]
     }
-  ]
-});
+  });
+
+  return {
+    ignoreWarnings: [
+      {
+        message: /mini-css-extract-plugin/
+      }
+    ]
+  };
+}
